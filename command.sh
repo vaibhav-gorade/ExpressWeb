f5536a86027d33c3a036e05c7be82c847c35c944
vaibhav-gorade <108924977+vaibhav-gorade@users.noreply.github.com>
2022-07-14 23:46:11 +0530

    new omkar file added

---
+++ command.sh
@@ -0,0 +1,7 @@
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+sudo yum install git -y
+npm install
+npm install mysql2
+npm install express
